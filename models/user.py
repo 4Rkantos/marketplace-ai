63059ab577e517cf9a0dba6d51e9ab1c8e34659d
Enzo Quartieri Adamo <75939399+EnzoQAdamo@users.noreply.github.com>
2024-11-29 19:03:12 -0300

    [FEAT] Criação da entidade Pedido

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from database.config import Base
 from pydantic import BaseModel, EmailStr
 
@@ -9,6 +10,9 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+
+    # Relacionamento One-to-Many entre User e Order
+    orders = relationship("Order", back_populates="user")
 
 # Schema base
 class UserBase(BaseModel):
